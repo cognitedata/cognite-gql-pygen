3cdb62ede8b608911ed9c14a0bdc7909333bef9c
anders-albert <anders0albert@gmail.com>
2023-04-12 13:23:17 +0200

    tests: Added failing test case for to_client_sdk

---
+++ cognite/gqlpygen/generator.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+
+def to_client_sdk(graphql_schema: str, client_name: str) -> dict[str, str]:
+    raise NotImplementedError()
--- tests/test_gqlpygen/test_generation.py
+++ tests/test_gqlpygen/test_generation.py
@@ -1,6 +1,7 @@
 import pytest
 
 from cognite.dm_clients.domain_modeling import DomainModel, Schema
+from cognite.gqlpygen.generator import to_client_sdk
 from examples.cinematography_domain.schema import cine_schema
 from tests.constants import CINEMATOGRAPHY
 
@@ -13,3 +14,23 @@ def generate_graphql_from_pydantic_data():
 @pytest.mark.parametrize("schema, expected_graphql", list(generate_graphql_from_pydantic_data()))
 def test_generate_graphql_from_pydantic(schema: Schema[DomainModel], expected_graphql: str):
     assert schema.as_str() == expected_graphql
+
+
+def generate_client_sdk_from_graphql_data():
+    input_schema = (CINEMATOGRAPHY / "schema.graphql").read_text()
+    client = (CINEMATOGRAPHY / "client.py").read_text()
+    schema = (CINEMATOGRAPHY / "schema.py").read_text()
+
+    yield pytest.param(
+        input_schema,
+        "CineClient",
+        {"client": client, "schema": schema},
+        id="Cinematography example",
+    )
+
+
+@pytest.mark.parametrize("graphql_schema, client_name, expected", list(generate_client_sdk_from_graphql_data()))
+def test_generate_client_sdk_from_graphql(graphql_schema: str, client_name: str, expected: dict[str, str]):
+    actual = to_client_sdk(graphql_schema, client_name)
+
+    assert actual == expected
